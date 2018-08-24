6b6455b9e3dd6418b661fe8b827bb056c0b1c80e
ganarogg <gena_gonzalez15@hotmail.com>
2018-08-23 23:50:06 -0400

    Variables principales

--- src/piramica/Piramica.java
+++ src/piramica/Piramica.java
@@ -1,12 +1,40 @@
 package piramica;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 /**
  *
  * @author Genarogg
  */
 
 public class Piramica {
-
+    JPanel jPanelSuperior,jPanelMedio;
+    JButton jBCalcular;
+    JTextArea jTextA;
+    
+    //Variables de introduccion de datos a la piramide:
+    
+    JTextField jT1;     
+    JTextField jT2;     
+    JTextField jT3;     
+    JTextField jT4;     
+    JTextField jT5;     
+    JTextField jT6;     
+    JTextField jT7;     
+    JTextField jT8;     
+    JTextField jT9;
+    JTextField jT11; 
+    JTextField jT10;
+    JTextField jT12;
+    JTextField jT13;     
+    JTextField jT14;
+    JTextField jT15;
+    JTextField jT16;
+    JTextField jT17;
+    JTextField jT18;
     
     
     public static void main(String[] args) {
